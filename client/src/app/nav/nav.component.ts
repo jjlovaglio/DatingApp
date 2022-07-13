0fef1c1926d3766eaeefc5099384112d8695158f
José Lovaglio <jjlovaglio@gmail.com>
2022-07-13 15:14:36 -0300

    ginish lec 58

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -8,18 +10,17 @@ import { AccountService } from '../_services/account.service';
 })
 export class NavComponent implements OnInit {
   model: any = {}
-  loggedIn: boolean;
-
-  constructor(private accountService: AccountService) { }
+  currentUser$: Observable<User>
+  
+  constructor(public accountService: AccountService) { }
 
   ngOnInit(): void {
-    this.getCurrentUser()
+    this.currentUser$ = this.accountService.currentUser$;
   }
 
   login() {
     this.accountService.login(this.model).subscribe(response => {
       console.log(response);
-      this.loggedIn = true;
     }, error => {
       console.log(error);
       
@@ -28,16 +29,8 @@ export class NavComponent implements OnInit {
 
   logout() {
     this.accountService.logout();
-    this.loggedIn = false;
   }
 
-  getCurrentUser() {
-    this.accountService.currentUser$.subscribe(user =>{
-      this.loggedIn = !!user;
-    }, error => {
-      console.log(error);
-      
-    })
-  }
+
 
 }
