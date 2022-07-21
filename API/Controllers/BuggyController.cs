d484d4e9ae5b85cd8ebcf4ee88627d8a7a8572d1
José Lovaglio <jjlovaglio@gmail.com>
2022-07-21 13:49:17 -0300

    finish lec 77

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -37,11 +37,19 @@ namespace API.Controllers
         [HttpGet("server-error")]
         public ActionResult<string> GetServerError()
         {
-            var thing = _context.Users.Find(-1);
+            try
+            {
 
-            var thingToReturn = thing.ToString();
-
-            return thingToReturn;
+                var thing = _context.Users.Find(-1);
+                var thingToReturn = thing.ToString();
+                return thingToReturn;
+                
+            }
+            catch (Exception ex)
+            {
+                
+                return StatusCode(500, "Computer says no!");
+            }
         }
 
         [HttpGet("bad-request")]
