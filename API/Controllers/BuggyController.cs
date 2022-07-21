a27c35311ecf2096a01acd033be7748b4352bd1d
José Lovaglio <jjlovaglio@gmail.com>
2022-07-21 15:35:22 -0300

    [bug] finish lec 79

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -1,5 +1,4 @@
-
-
+using System;
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Authorization;
@@ -7,10 +6,9 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    public class BuggyController: BaseApiController
+    public class BuggyController : BaseApiController
     {
         private readonly DataContext _context;
-
         public BuggyController(DataContext context)
         {
             _context = context;
@@ -27,27 +25,26 @@ namespace API.Controllers
         public ActionResult<AppUser> GetNotFound()
         {
             var thing = _context.Users.Find(-1);
-         
+
             if (thing == null) return NotFound();
 
             return Ok(thing);
-
         }
 
         [HttpGet("server-error")]
         public ActionResult<string> GetServerError()
         {
-  
             var thing = _context.Users.Find(-1);
+
             var thingToReturn = thing.ToString();
+
             return thingToReturn;
-            
         }
 
         [HttpGet("bad-request")]
         public ActionResult<string> GetBadRequest()
         {
-            return BadRequest("This was a bad request"); 
+            return BadRequest("This was not a good request");
         }
     }
 }
--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { HomeComponent } from './home/home.component';
 import { ListsComponent } from './lists/lists.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
@@ -10,7 +11,7 @@ import { AuthGuard } from './_guards/auth.guard';
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {
-    path: '', 
+    path: '',
     runGuardsAndResolvers: 'always',
     canActivate: [AuthGuard],
     children: [
@@ -20,6 +21,7 @@ const routes: Routes = [
       {path: 'messages', component: MessagesComponent},
     ]
   },
+  {path: 'errors', component: TestErrorsComponent},
   {path: '**', component: HomeComponent, pathMatch: 'full'},
 
 ];
