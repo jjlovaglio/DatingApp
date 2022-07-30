85444eeeb66feac0956bf30e37fdb8d45c9d21f8
José Lovaglio <jjlovaglio@gmail.com>
2022-07-30 11:26:16 -0300

    fin lec 97

--- API/DTOs/MemberDto.cs
+++ API/DTOs/MemberDto.cs
@@ -8,8 +8,9 @@ namespace API.DTOs
     public class MemberDto
     {
             public int Id { get; set; }
-        public string UserName { get; set; }
-        public DateTime Age { get; set; }
+        public string Username { get; set; }
+        public string PhotoUrl { get; set; }
+        public int Age { get; set; }
         public string KnownAs { get; set; }
         public DateTime Created { get; set; } 
         public DateTime LastActive { get; set; } 
