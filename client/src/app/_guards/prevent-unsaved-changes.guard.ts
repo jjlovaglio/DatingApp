e53cab49d1142f3f32fce8c2dd893c46e97b5be2
José Lovaglio <jjlovaglio@gmail.com>
2022-08-08 18:43:19 -0300

    real fin lec 118

---
+++ client/src/app/_guards/prevent-unsaved-changes.guard.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, CanDeactivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { MemberEditComponent } from '../members/member-edit/member-edit.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PreventUnsavedChangesGuard implements CanActivate, CanDeactivate<unknown> {
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return true;
+  }
+  canDeactivate(component: MemberEditComponent): boolean {
+    return true;
+  }
+
+}
