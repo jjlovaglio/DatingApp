7121bb465710e56e87295baf76bb6d6a35ae9ca7
José Lovaglio <jjlovaglio@gmail.com>
2022-08-15 10:48:01 -0300

    [fix] lec 138

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -42,6 +42,7 @@ export class AccountService {
     }
 
     setCurrentUser(user: User) {
+      localStorage.setItem('user', JSON.stringify(user));
       this.currentUserSource.next(user);
     }
 
