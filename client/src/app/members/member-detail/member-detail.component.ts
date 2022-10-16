c51d6d95efa35e854379a62782a02383823cdef9
José Lovaglio <jjlovaglio@gmail.com>
2022-10-16 18:57:07 -0300

    fin lec 194

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -13,7 +13,7 @@ import { MessageService } from 'src/app/_services/message.service';
   styleUrls: ['./member-detail.component.css']
 })
 export class MemberDetailComponent implements OnInit {
-  @ViewChild('memberTabs') memberTabs: TabsetComponent;
+  @ViewChild('memberTabs', {static: true}) memberTabs: TabsetComponent;
   galleryOptions: NgxGalleryOptions[];
   galleryImages: NgxGalleryImage[];
   member: Member;
@@ -26,6 +26,9 @@ export class MemberDetailComponent implements OnInit {
 
   ngOnInit(): void {
     this.loadMember();
+    this.route.queryParams.subscribe(params => {
+      params.tab ? this.selectTab(params.tab) : this.selectTab(0);
+    });
     this.galleryOptions = [
       {
         width: '500px',
@@ -37,6 +40,7 @@ export class MemberDetailComponent implements OnInit {
 
       }
     ]
+
   }
 
   getImages(): NgxGalleryImage[] {
@@ -71,5 +75,9 @@ export class MemberDetailComponent implements OnInit {
     if(this.activeTab.heading === 'Messages' && this.messages.length === 0) {
       this.loadMessages();
     }
+  }
+
+  selectTab(tabId: number) {
+    this.memberTabs.tabs[tabId].active = true;
   }
 }
