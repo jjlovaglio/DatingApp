19450f7b0f30d6c093c23f08dcc7dec0dfa91c47
José Lovaglio <jjlovaglio@gmail.com>
2022-10-24 13:54:57 -0300

    fin lec 216

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -31,6 +31,8 @@ import { TimeagoModule } from 'ngx-timeago';
 import { MemberMessagesComponent } from './members/member-messages/member-messages.component';
 import { AdminPanelComponent } from './admin/admin-panel/admin-panel.component';
 import { HasRoleDirective } from './_directives/has-role.directive';
+import { UserManagementComponent } from './admin/user-management/user-management.component';
+import { PhotoManagementComponent } from './admin/photo-management/photo-management.component';
 
 @NgModule({
   declarations: [
@@ -53,6 +55,8 @@ import { HasRoleDirective } from './_directives/has-role.directive';
     MemberMessagesComponent,
     AdminPanelComponent,
     HasRoleDirective,
+    UserManagementComponent,
+    PhotoManagementComponent,
   ],
   imports: [
     BrowserModule,
